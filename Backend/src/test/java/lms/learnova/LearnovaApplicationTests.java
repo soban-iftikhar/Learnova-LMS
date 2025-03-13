369b7dbdbc0cf81f0bae764538059155b5fa6331
Soban Iftikhar <sobaniftikhar1098@gmail.com>
2025-03-13 07:36:36 +0500

    Initial commit - Full project structure

---
+++ Backend/src/test/java/lms/learnova/LearnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package lms.learnova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
